a292837be3f4a88f73846a51c216c8ffdb8cf451
luca <l.giovannesi@gmail.com>
2021-03-31 12:27:59 +0200

    Web interface

---
+++ WebInterface/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <Title msg="RIOT Greenhouse"/>
+    <Sensor/>
+
+  </div>
+</template>
+
+<script>
+import Title from './components/Title.vue'
+import Sensor from "./components/Sensors";
+
+export default {
+  name: 'App',
+  components: {
+    Title, Sensor
+  }
+}
+</script>
+
+<style>
+button {
+  margin: 10px;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 15px;
+}
+</style>
