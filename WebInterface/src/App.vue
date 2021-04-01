d1250829be7c162249454532cd95b117cea42516
luca <l.giovannesi@gmail.com>
2021-04-01 10:48:27 +0200

    Button open on github

--- WebInterface/src/App.vue
+++ WebInterface/src/App.vue
@@ -2,18 +2,20 @@
   <div id="app">
     <Title msg="RIOT Greenhouse"/>
     <Sensor/>
-
+    <github-button href="https://github.com/kernel-machine/RIOT-GreenHouse" data-size="large">Open on GitHub
+    </github-button>
   </div>
 </template>
 
 <script>
 import Title from './components/Title.vue'
 import Sensor from "./components/Sensors";
+import GithubButton from 'vue-github-button'
 
 export default {
   name: 'App',
   components: {
-    Title, Sensor
+    Title, Sensor, GithubButton
   }
 }
 </script>
