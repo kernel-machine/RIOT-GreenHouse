d7979f58f8aacd5a3c4eab0592388dd289758210
kernel-machine <l.giovannesi@gmail.com>
2021-05-15 10:55:15 +0200

    Fixed graph error

--- WebInterface/src/components/SensorSelector.vue
+++ WebInterface/src/components/SensorSelector.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <loading :active.sync="isLoading"
+             :can-cancel="false"
+             :is-full-page="false"></loading>
     <a>Aggregated values by device</a>
 
     <MinMaxAvg title="Temperature" unit="°C" color="#ef476f"
@@ -11,25 +14,25 @@
     <MinMaxAvg title="Soil moisture" unit="%" color="#fed67b"
                v-bind:data="received_data.map(x=>x.water_level)"></MinMaxAvg>
     <a>Select a sensor </a>
-    <select v-model="selectedSensor" v-if="this.received_data.length>0">
+    <select v-model="selectedSensor" v-if="received_data.length>0">
       <option value="1" selected="true">Temperature</option>
       <option value="2">Humidity</option>
       <option value="3">Soil</option>
       <option value="4">Water level</option>
     </select>
-    <div v-if="this.received_data.length>0" class="flex_container">
-      <my-graph class="graph" v-bind:values="this.received_data.filter(x=>x.device_id==4).map(mapBySensor)"
-                v-bind:ts="this.received_data.filter(x=>x.device_id==4).map(x=>x.timestamp)" name="Device 4"
+    <div class="flex_container">
+      <my-graph class="graph" v-bind:values="received_data.filter(x=>x.device_id==4).map(mapBySensor)"
+                v-bind:ts="received_data.filter(x=>x.device_id==4).map(x=>x.timestamp)" name="Device 4"
                 color="#ef476f">
       </my-graph>
 
-      <my-graph class="graph" v-bind:values="this.received_data.filter(x=>x.device_id==5).map(mapBySensor)"
-                v-bind:ts="this.received_data.filter(x=>x.device_id==5).map(x=>x.timestamp)" name="Device 5"
+      <my-graph class="graph" v-bind:values="received_data.filter(x=>x.device_id==5).map(mapBySensor)"
+                v-bind:ts="received_data.filter(x=>x.device_id==5).map(x=>x.timestamp)" name="Device 5"
                 color="#e05a10">
       </my-graph>
 
-      <my-graph class="graph" v-bind:values="this.received_data.filter(x=>x.device_id==6).map(mapBySensor)"
-                v-bind:ts="this.received_data.filter(x=>x.device_id==6).map(x=>x.timestamp)" name="Device 6"
+      <my-graph class="graph" v-bind:values="received_data.filter(x=>x.device_id==6).map(mapBySensor)"
+                v-bind:ts="received_data.filter(x=>x.device_id==6).map(x=>x.timestamp)" name="Device 6"
                 color="#fed67b">
       </my-graph>
     </div>
@@ -40,31 +43,34 @@
 import axios from "axios";
 import MinMaxAvg from "./MinMaxAvg";
 import MyGraph from "@/components/MyGraph";
+import Loading from "vue-loading-overlay";
 
 export default {
   name: "SensorSelector",
-  components: {MinMaxAvg, MyGraph},
+  components: {MinMaxAvg, MyGraph, Loading},
   data() {
     return {
       selectedSensor: 1,
-      received_data: []
+      received_data: [],
+      received_data_by_device: [],
+      isLoading: true
     }
   },
   watch: {
     selectedSensor: function () {
-      console.log("Selected ", this.selectedSensor)
-      this.selectedSensor=Number(this.selectedSensor)
+      this.selectedSensor = Number(this.selectedSensor)
     }
   },
   mounted() {
     this.loadData()
+    setInterval(this.loadData, 60 * 1000)
   },
   methods: {
     loadData() {
       axios.get("https://a88wox1sp3.execute-api.us-east-1.amazonaws.com/default/getSensorValues")
           .then(d => {
-            console.log(d.data)
             this.received_data = d.data.Items
+            this.isLoading = false
           })
     },
     mapBySensor(x) {
