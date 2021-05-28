33ea6e6cc519d5f1458210437a162c728b627601
Luca Giovannesi <l.giovannesi@gmail.com>
2021-05-28 13:24:30 +0200

    Update third assignment page

--- src/WebInterface/src/Pages/ThirdAssignmentPage.vue
+++ src/WebInterface/src/Pages/ThirdAssignmentPage.vue
@@ -3,39 +3,32 @@
     <loading :active.sync="isLoading"
              :can-cancel="false"
              :is-full-page="true"></loading>
-    <!--
-    <MinMaxAvg title="Temperature" unit="°C" :color="this.TEMP_COLOR"
-               v-bind:data="this.$data.received_data.map(x=>x.temperature)"></MinMaxAvg>
-    <MinMaxAvg title="Humidity" unit="%" :color="this.HUMIDITY_COLOR"
-               v-bind:data="this.$data.received_data.map(x=>x.humidity)"></MinMaxAvg>
-    <MinMaxAvg title="Soil moisture" unit="%" :color="this.SOIL_COLOR"
-               v-bind:data="this.$data.received_data.map(x=>x.soil)"></MinMaxAvg>
-               -->
-    <h3 :style="{color:this.TEMP_COLOR}"><b>Temperature</b></h3>
-    <p>
-      <b>min:</b> {{ this.received_data.temp_min }} °C
-      <b>avg:</b> {{ this.received_data.temp_max }} °C
-      <b>max:</b> {{ this.received_data.temp_avg }} °C
-    </p>
+    <div v-if="!isLoading">
+      <h3 :style="{color:this.TEMP_COLOR}"><b>Temperature</b></h3>
+      <p>
+        <b>min:</b> {{ this.received_data.temp_min }} °C
+        <b>avg:</b> {{ this.received_data.temp_max }} °C
+        <b>max:</b> {{ this.received_data.temp_avg }} °C
+      </p>
 
-    <h3 :style="{color:this.HUMIDITY_COLOR}"><b>Humidity</b></h3>
-    <p>
-      <b>min:</b> {{ this.received_data.hum_min }} %
-      <b>avg:</b> {{ this.received_data.hum_max }} %
-      <b>max:</b> {{ this.received_data.hum_avg }} %
-    </p>
+      <h3 :style="{color:this.HUMIDITY_COLOR}"><b>Humidity</b></h3>
+      <p>
+        <b>min:</b> {{ this.received_data.hum_min }} %
+        <b>avg:</b> {{ this.received_data.hum_max }} %
+        <b>max:</b> {{ this.received_data.hum_avg }} %
+      </p>
 
-    <h3 :style="{color:this.SOIL_COLOR}"><b>Soil moisture</b></h3>
-    <p>
-      <b>min:</b> {{ this.received_data.soil_min }} %
-      <b>avg:</b> {{ this.received_data.soil_max }} %
-      <b>max:</b> {{ this.received_data.soil_avg }} %
-    </p>
-    <br>
-    <p>
-      <b>Last update: </b>{{ this.formatData(this.received_data.last_update) }}
-    </p>
-
+      <h3 :style="{color:this.SOIL_COLOR}"><b>Soil moisture</b></h3>
+      <p>
+        <b>min:</b> {{ this.received_data.soil_min }} %
+        <b>avg:</b> {{ this.received_data.soil_max }} %
+        <b>max:</b> {{ this.received_data.soil_avg }} %
+      </p>
+      <br>
+      <p>
+        <b>Last update: </b>{{ this.formatData(this.received_data.last_update) }}
+      </p>
+    </div>
   </div>
 </template>
 
